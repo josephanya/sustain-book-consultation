b5f04337702b1768a1196d631edd9158edb92697
feyifoluwa <abioyefeyifoluwa@gmail.com>
2021-01-14 22:53:55 +0100

    send form data to slack channel

--- script.js
+++ script.js
@@ -4,11 +4,14 @@ var date = tomorrow.toString().slice(0, 15);
 
 var select = document.getElementById('booking-date');
 var bookButton = document.getElementsByClassName('book-button')[0];
+let success = document.getElementById('success');
+let content = document.getElementById('content');
 
 var option = document.createElement('option');
 option.appendChild(document.createTextNode(date));
 option.setAttribute('value', date);
 select.appendChild(option);
+
 
 const submitForm = () => {
     var fullName = document.getElementById('full-name').value;
@@ -18,12 +21,33 @@ const submitForm = () => {
     var bookingDate = document.getElementById('booking-date').value;
 
     var bookingData = {
-        'Full name' : fullName,
-        'Email' : email,
-        'Phone' : phone,
-        'Booking date' : bookingDate,
-        'Booking time' : bookingTime,
+        'fullName' : fullName,
+        'email' : email,
+        'phone' : phone,
+        'bookingDate' : bookingDate,
+        'bookingTime' : bookingTime,
     }
+    let urL = "https://hooks.slack.com/services/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"
+    let payload = {"text": `New consultation for ${bookingData.fullName} on ${bookingData.bookingDate} at ${bookingData.bookingTime}. Email: ${bookingData.email}, phone no: ${bookingData.phone}`}
+    let otherParam = {
+        method: 'POST',
+        body: JSON.stringify(payload)
+    }
+    
+    fetch(`https://cors-anywhere.herokuapp.com/${urL}`, otherParam)
+    .then(res=> {
+        if(res.status == 200){
+            content.classList.add('hide');
+            success.classList.remove('hide')
+            success.classList.add('show');
+
+        }
+        return res.json()
+    })
+    .then(data=> console.log(data))
+    .catch(err=> console.log(err))
+    
 }
+
 
 bookButton.addEventListener('click', submitForm);
